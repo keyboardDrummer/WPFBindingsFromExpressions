36f5379c43adfbce15e791e8d632aaec32b4cfdc
Remy Willems <remy.willems@ortec-finance.com>
2017-01-23 00:24:47 +0100

    Rename.

--- WPFBindingGeneration/ExpressionBindings/ExpressionToBindingParser.cs
+++ WPFBindingGeneration/ExpressionBindings/ExpressionToBindingParser.cs
@@ -54,7 +54,7 @@ namespace WPFBindingGeneration
 			{
 				var arrayParameter = Expression.Parameter(typeof(object[]));
 				var pathIndices = paths.Select((e, i) => Tuple.Create(e, i)).
-					ToDictionary(t => t.Item1, t => t.Item2, PathsAndReduceExpression<Expression>.Comparer);
+					ToDictionary(t => t.Item1, t => t.Item2, PathsAndPathReducer<Expression>.Comparer);
 
 				var arrayParameterBody = result.CreateExpression((path, type) => GetArrayParameter(arrayParameter, pathIndices[path], type));
 
@@ -77,18 +77,18 @@ namespace WPFBindingGeneration
 		/// <summary>
 		/// Returns an updated expression with all the paths replaced by parameters.
 		/// </summary>
-		private static PathsAndReduceExpression<Expression> ExtractPaths(Expression expression)
+		private static PathsAndPathReducer<Expression> ExtractPaths(Expression expression)
 		{
 			var constant = expression as ConstantExpression;
 			if (constant != null)
 			{
-				return new PathsAndReduceExpression<Expression>(p => constant);
+				return new PathsAndPathReducer<Expression>(p => constant);
 			}
 
 			var path = PathExpressions.ParsePath(expression);
 			if (path != null)
 			{
-				return new PathsAndReduceExpression<Expression>(c => c(path, expression.Type), path);
+				return new PathsAndPathReducer<Expression>(c => c(path, expression.Type), path);
 			}
 
 			var binaryExpression = expression as BinaryExpression;
@@ -132,7 +132,7 @@ namespace WPFBindingGeneration
 			throw new NotImplementedException();
 		}
 
-		private static PathsAndReduceExpression<Expression> ParseConditional(ConditionalExpression conditional)
+		private static PathsAndPathReducer<Expression> ParseConditional(ConditionalExpression conditional)
 		{
 			var conditionResult = ExtractPaths(conditional.Test);
 			var thenResult = ExtractPaths(conditional.IfTrue);
@@ -141,15 +141,15 @@ namespace WPFBindingGeneration
 				(thenAndElse, condition) => (Expression)Expression.Condition(condition, thenAndElse.Item1, thenAndElse.Item2, conditional.Type));
 		}
 
-		private static PathsAndReduceExpression<Expression> ParseInvocation(InvocationExpression invoke)
+		private static PathsAndPathReducer<Expression> ParseInvocation(InvocationExpression invoke)
 		{
 			var argumentResults = invoke.Arguments.Select(ExtractPaths);
-			var argumentsResult = PathsAndReduceExpression<Expression>.Flatten(argumentResults);
+			var argumentsResult = PathsAndPathReducer<Expression>.Flatten(argumentResults);
 			var expressionResult = ExtractPaths(invoke.Expression);
 			return expressionResult.Combine(argumentsResult, (newObject, newArguments) => (Expression)Expression.Invoke(newObject, newArguments));
 		}
 
-		private static PathsAndReduceExpression<Expression> ParseBinaryType(TypeBinaryExpression binaryType)
+		private static PathsAndPathReducer<Expression> ParseBinaryType(TypeBinaryExpression binaryType)
 		{
 			return ExtractPaths(binaryType.Expression).Select<Expression>(newExpression =>
 			{
@@ -167,7 +167,7 @@ namespace WPFBindingGeneration
 			});
 		}
 
-		private static PathsAndReduceExpression<Expression> ParseBinary(BinaryExpression binaryExpression)
+		private static PathsAndPathReducer<Expression> ParseBinary(BinaryExpression binaryExpression)
 		{
 			var leftResult = ExtractPaths(binaryExpression.Left);
 			var rightResult = ExtractPaths(binaryExpression.Right);
@@ -175,12 +175,12 @@ namespace WPFBindingGeneration
 				binaryExpression.IsLiftedToNull, binaryExpression.Method));
 		}
 
-		private static PathsAndReduceExpression<Expression> ParseMethodCall(MethodCallExpression methodCall)
+		private static PathsAndPathReducer<Expression> ParseMethodCall(MethodCallExpression methodCall)
 		{
 			var argumentResults = methodCall.Arguments.Select(ExtractPaths);
-			var argumentsResult = PathsAndReduceExpression<Expression>.Flatten(argumentResults);
+			var argumentsResult = PathsAndPathReducer<Expression>.Flatten(argumentResults);
 			var objectResult = methodCall.Object == null
-				? new PathsAndReduceExpression<Expression>(f => null)
+				? new PathsAndPathReducer<Expression>(f => null)
 				: ExtractPaths(methodCall.Object);
 			return objectResult.Combine(argumentsResult, (newObject, newArguments) => (Expression)Expression.Call(newObject, methodCall.Method, newArguments));
 		}
--- WPFBindingGeneration/ExpressionBindings/PathsAndPathReducer.cs
+++ WPFBindingGeneration/ExpressionBindings/PathsAndPathReducer.cs
@@ -6,32 +6,19 @@ using WPFBindingGeneration.ExpressionBindings.Paths;
 
 namespace WPFBindingGeneration
 {
-	class ToStringEqualityComparer<T> : IEqualityComparer<T>
-	{
-		public bool Equals(T x, T y)
-		{
-			return x.ToString() == y.ToString();
-		}
-
-		public int GetHashCode(T obj)
-		{
-			return obj.ToString().GetHashCode();
-		}
-	}
-
 	delegate Expression CreatePathParameter(IPathExpression path, Type type);
 
 	/// <summary>
 	/// Contains a set of paths and a function to build a T that takes the results of those paths as input.
 	/// </summary>
-	class PathsAndReduceExpression<T>
+	class PathsAndPathReducer<T>
 	{
-		public PathsAndReduceExpression(Func<CreatePathParameter, T> createExpression, params IPathExpression[] paths)
+		public PathsAndPathReducer(Func<CreatePathParameter, T> createExpression, params IPathExpression[] paths)
 			: this(createExpression, new Utility.SortedSet<IPathExpression>(Comparer, paths))
 		{
 		}
 
-		public PathsAndReduceExpression(Func<CreatePathParameter, T> createExpression, Utility.SortedSet<IPathExpression> paths)
+		public PathsAndPathReducer(Func<CreatePathParameter, T> createExpression, Utility.SortedSet<IPathExpression> paths)
 		{
 			Paths = paths;
 			CreateExpression = createExpression;
@@ -49,9 +36,9 @@ namespace WPFBindingGeneration
 			get;
 		}
 
-		public static PathsAndReduceExpression<IEnumerable<T>> Flatten(IEnumerable<PathsAndReduceExpression<T>> input)
+		public static PathsAndPathReducer<IEnumerable<T>> Flatten(IEnumerable<PathsAndPathReducer<T>> input)
 		{
-			var seed = new PathsAndReduceExpression<IEnumerable<T>>(c => Enumerable.Empty<T>());
+			var seed = new PathsAndPathReducer<IEnumerable<T>>(c => Enumerable.Empty<T>());
 			return input.Aggregate(seed, (results, result) => results.Combine(result, (items, item) =>
 			{
 			    IEnumerable<T> enumerable = items.Concat(new[] {item});
@@ -59,18 +46,18 @@ namespace WPFBindingGeneration
 			}));
 		}
 
-		public PathsAndReduceExpression<R> Combine<U, R>(PathsAndReduceExpression<U> other, Func<T, U, R> merge)
+		public PathsAndPathReducer<R> Combine<U, R>(PathsAndPathReducer<U> other, Func<T, U, R> merge)
 		{
 			var combinedPaths = new Utility.SortedSet<IPathExpression>(Comparer);
 			foreach (var item in other.Paths.Concat(Paths))
 				combinedPaths.Add(item);
 
-			return new PathsAndReduceExpression<R>(c => merge(CreateExpression(c), other.CreateExpression(c)), combinedPaths);
+			return new PathsAndPathReducer<R>(c => merge(CreateExpression(c), other.CreateExpression(c)), combinedPaths);
 		}
 
-		public PathsAndReduceExpression<U> Select<U>(Func<T, U> func)
+		public PathsAndPathReducer<U> Select<U>(Func<T, U> func)
 		{
-			return new PathsAndReduceExpression<U>(c => func(CreateExpression(c)), Paths);
+			return new PathsAndPathReducer<U>(c => func(CreateExpression(c)), Paths);
 		}
 	}
 }
